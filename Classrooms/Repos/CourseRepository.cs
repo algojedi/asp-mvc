f7c47ee92cfe4edc8a5cd4996c259d72a85fde33
banwsukh <ssbanwait78@gmail.com>
2020-09-17 20:18:11 -0400

    clean up

--- Classrooms/Repos/CourseRepository.cs
+++ Classrooms/Repos/CourseRepository.cs
@@ -1,16 +1,14 @@
 using Classrooms.Data;
 using Classrooms.Models;
-using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
 
 //public class CourseRepository : ICourseRepository
 public class CourseRepository : Repository<Course>, ICourseRepository
 {
     private readonly SchoolContext _context;
-    public CourseRepository(SchoolContext context) :base(context)
+
+    public CourseRepository(SchoolContext context) : base(context)
     {
         _context = context;
     }
@@ -22,36 +20,15 @@ public class CourseRepository : Repository<Course>, ICourseRepository
     }
 
     // returns a list of every person enrolled in the course
-    //public IEnumerable<string> GetMemberNames(string courseId)
     public IEnumerable<string> GetMemberIds(string courseId)
     {
         //var enrollments = _context.Enrollments.Where(e => e.CourseID == courseId).Select(enrollment => enrollment.PersonID);
 
         IEnumerable<string> query = (from u in _context.Users
-                                         join e in _context.Enrollments on u.Id equals e.PersonID
-                                         select e.PersonID).ToList();
-                      //select new { u.UserName }).ToList();
-        //select new List<string> { u.UserName };
-        //      Dim customerList =
-        //        From cust In db.Customers
-        //        Where cust.CompanyName.StartsWith("L")
-        //Select New CustomerInfo With {.CompanyName = cust.CompanyName,
-        //                              .ContactName = cust.ContactName}
-
-        //      DataGridView1.DataSource = customerList
-
-
-
-        //List<Point> lp = lpf.ConvertAll(
-        //    new Converter<PointF, Point>(PointFToPoint));
-
-
-        //foreach (var pid in enrollments)
-        //{
-        //    peopleInCourse.Add(_context.Users.Find(pid).UserName);
-        //}
-        //return peopleInCourse;
-        return query;
+                                     join e in _context.Enrollments on u.Id equals e.PersonID
+                                     where e.CourseID == courseId
+                                     select e.PersonID).ToList();
+               return query;
     }
 
     // get a list of the courses a person is enrolled in
